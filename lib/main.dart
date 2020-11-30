2f2cb898f42fd343ea6e32ad0129d3d1d26d3865
glorypuncuna <33197448+glorypuncuna@users.noreply.github.com>
2020-11-30 13:46:42 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:forui/screens/wrapper.dart';
-import 'package:forui/services/auth.dart';
-import 'package:forui/models/user.dart';
+import 'formForum.dart';
+import 'mainForum.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper(),
-        theme: ThemeData(primarySwatch: Colors.grey),
-      ),
+    return MaterialApp(
+      home: Forum(),
+      title: 'Forum',
+      routes: <String, WidgetBuilder>{
+        '/formForum': (BuildContext context) => Forum(),
+        '/forum': (BuildContext context) => ForumDiscussion(),
+      },
     );
   }
 }
